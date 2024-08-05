859647e09f9947d6dd7cfdb6649462adf8004fd3
Irendida Bautista <irendidabh@gmail.com>
2024-08-04 22:27:18 -0700

    Estructura del proyecto, config de webpack e inicio de la estructura HTML

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',// Punto de entrada de tu aplicacion
+    output: {
+        filename: 'bundle.js', //Nombre del archivo de salida
+        path: path.resolver(__dirname, 'dist'), //Carpeta de salida
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/, //regex para identificar archivos
+                use: ['style-loader', 'css-loader'], //Loaders para procesar archivos
+            },
+            {
+              test: /\.js$/, //Regex para identificar archivos JS
+              exclude: /node_modules/, //Excluir la carpeta node_modules
+              use: {
+                loader: 'babel-loader', //Loader para convertir JS moderno al JS compatible a mas navegadores
+                options: {
+                    presets:['@babel/preset-env'],
+                },
+              },   
+            },
+        ],
+    },
+    devtool: 'source-map',//Genera source maps para facilitar la depuración
+    devServer: {
+        contentBase: path.resolve(__dirname,'dist'), //Carpeta del que correra el servidor
+        compress: true, // Habilitar compresion gzip
+        port: 9000, //Puerto del servidor de desarrollo
+    },
+};
